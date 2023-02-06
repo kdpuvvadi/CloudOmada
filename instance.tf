26daed17e536cd890b420a50de06a706a6e36976
KD Puvvadi <rao@puvvadi.me>
2023-02-06 14:54:54 +0530

    required_version

--- instance.tf
+++ instance.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "omada_instance" {
   subnet_id                   = aws_subnet.omada_vpc_Subnet.id
   associate_public_ip_address = true
   key_name                    = aws_key_pair.omada_key.key_name
-  vpc_security_group_ids = [ aws_security_group.Omada_Security_Group.id ]
+  vpc_security_group_ids      = [aws_security_group.Omada_Security_Group.id]
 
   root_block_device {
     delete_on_termination = true
--- variables.tf
+++ variables.tf
@@ -4,21 +4,26 @@ variable "region" {
 }
 
 variable "vpc_CIDRblock" {
+  type        = string
   description = "CIDR block for VPC"
 }
 
 variable "instance_tenancy" {
+  type        = string
   description = "Tenancy of instance" # Keep it default to save cost.
 }
 
 variable "subnet_CIDRblock" {
+  type        = string
   description = "CIDR block for subnet"
 }
 
 variable "availability_Zone" {
+  type        = string
   description = "availability zone"
 }
 
 variable "instance_size" {
+  type        = string
   description = "Instance Size"
 }
